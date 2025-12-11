234278f590664905f5c05920fa76cfef0264c664
yurcee <148818082+yurcee@users.noreply.github.com>
2025-12-11 01:06:28 +0100

    Update ComfyUI-Manager repository URL for shell script
    This was the problem in the original repo that it cloned the live updated original ComfyUI Manager

--- install_comfyui-cpu.sh
+++ install_comfyui-cpu.sh
@@ -4,5 +4,5 @@ source venv/bin/activate
 pip3 install torch==2.4.1+cpu torchvision==0.19.1+cpu torchaudio==2.4.1+cpu --index-url https://download.pytorch.org/whl/cpu
 pip3 install -r requirements.txt
 cd custom_nodes || exit
-git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+git clone https://github.com/yurcee/ComfyUI-Manager.git
 python3 main.py --cpu --cpu-vae
